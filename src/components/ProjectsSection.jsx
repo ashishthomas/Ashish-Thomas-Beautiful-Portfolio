a7695c01df7ff91cc239c29074674b5c9319c62e
ashish <ashishthomas1995@gmail.com>
2025-07-31 18:02:31 +0530

    updated projects section and the image in the portfolio

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -7,7 +7,7 @@ const projects = [
     description:
       "A modern, user-friendly e-commerce app for beauty products with authentication, reviews, cart, and seamless shopping experience",
     image: "/projects/Kira-project.png",
-    tags: ["React", "Typescript", "TailwindCSS"],
+    tags: ["React", "Typescript", "TailwindCSS", "vite"],
     demoUrl: "#",
     githubUrl: "https://github.com/ashishthomas/Kira-Beauty-Ecommerce.git",
   },
@@ -17,7 +17,7 @@ const projects = [
     description:
       "A modern, user-friendly app for managing books, onboarding checklists, and user support with role-based-routing built using React. ",
     image: "/projects/Trade-Excelsior-project.png",
-    tags: ["React", "RBRouting", "MUI"],
+    tags: ["React", "RBRouting", "MUI", "vite"],
     demoUrl: "#",
     githubUrl: "https://github.com/ashishthomas/Trade-Excelsior.git",
   },
@@ -27,7 +27,7 @@ const projects = [
     description:
       "A web platform showcasing menu, services, events and online reservations for a modern restaurant experience",
     image: "/projects/Sizzling_Delights_Website.jpg",
-    tags: ["HTML", "CSS", "Bootstrap", "Js"],
+    tags: ["HTML", "CSS", "Bootstrap", "JavaScript"],
     demoUrl: "#",
     githubUrl:
       "https://github.com/ashishthomas/Sizzling-Restaurant-Website.git",
@@ -38,7 +38,7 @@ const projects = [
     description:
       "A modern online job portal for job seekers and employers, featuring job listings, applications, filtering, and user authentication.",
     image: "/projects/Online-Dream-Career-Portal.png",
-    tags: ["React", "TypeScript", "Tailwind CSS"],
+    tags: ["React", "TypeScript", "Tailwind CSS", "vite"],
     demoUrl: "#",
     githubUrl: "https://github.com/ashishthomas/Online-Job-Portal.git",
   },
@@ -48,9 +48,20 @@ const projects = [
     description:
       "A modern payment application enabling secure transactions, fee calculation, and currency management with user-friendly dashboards for personal and business users.",
     image: "/projects/FlowPay-Payment-Application.png",
-    tags: ["React", "TypeScript", "Tailwind CSS"],
+    tags: ["React", "TypeScript", "Tailwind CSS", "vite"],
     demoUrl: "#",
     githubUrl: "https://github.com/ashishthomas/Payment-flow-app.git",
+  },
+  {
+    id: 6,
+    title: "Nexora Technologies Company Website",
+    description:
+      "A modern corporate website for Nexora Technologies, showcasing services, portfolio, and expertise with responsive design, fast performance, and seamless user experience for clients and partners.",
+    image: "/projects/Nexora-Technologies-Corporate-Website.png",
+    tags: ["React", "Next.js", "Tailwind CSS", "vercel"],
+    demoUrl: "#",
+    githubUrl:
+      "https://github.com/ashishthomas/Nexora-Technologies-Corporate-Website.git",
   },
 ];
 

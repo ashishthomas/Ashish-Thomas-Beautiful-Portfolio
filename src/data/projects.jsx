467642d4cf14ae5de0105f56c5a3a19c95b53a6b
ashishthomas <ashishthomas1995@gmail.com>
2025-11-26 10:17:20 +0530

    projects data updated, removed few projects from the object

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,16 +1,6 @@
 export const projects = [
   {
     id: 1,
-    title: "Kira Ecommerce Web App",
-    description:
-      "A modern, user-friendly e-commerce app for beauty products with authentication, reviews, cart, and seamless shopping experience",
-    image: "/projects/Kira-project.png",
-    tags: ["React.Js", "Typescript", "TailwindCSS", "vite"],
-    demoUrl: "#",
-    githubUrl: "https://github.com/ashishthomas/Kira-Beauty-Ecommerce.git",
-  },
-  {
-    id: 2,
     title: "Trade Excelsior Web App",
     description:
       "A modern, user-friendly app for managing books, onboarding checklists, and user support with role-based-routing built using React.",
@@ -20,28 +10,17 @@ export const projects = [
     githubUrl: "https://github.com/ashishthomas/Trade-Excelsior.git",
   },
   {
+    id: 2,
+    title: "Kira Ecommerce Web App",
+    description:
+      "A modern, user-friendly e-commerce app for beauty products with authentication, reviews, cart, and seamless shopping experience",
+    image: "/projects/Kira-project.png",
+    tags: ["React.Js", "Typescript", "TailwindCSS", "vite"],
+    demoUrl: "#",
+    githubUrl: "https://github.com/ashishthomas/Kira-Beauty-Ecommerce.git",
+  },
+  {
     id: 3,
-    title: "Sizzling Delights Restaurant Website",
-    description:
-      "A web platform showcasing menu, services, events and online reservations for a modern restaurant experience",
-    image: "/projects/Sizzling_Delights_Website.jpg",
-    tags: ["HTML", "CSS", "Bootstrap", "JavaScript"],
-    demoUrl: "#",
-    githubUrl:
-      "https://github.com/ashishthomas/Sizzling-Restaurant-Website.git",
-  },
-  {
-    id: 4,
-    title: "Online Job Portal Application",
-    description:
-      "A modern job portal enabling seekers and employers to connect through listings, applications, filtering, and secure authentication for efficient and streamlined recruitment.",
-    image: "/projects/Online-Dream-Career-Portal.png",
-    tags: ["React.Js", "TypeScript", "Tailwind CSS", "vite"],
-    demoUrl: "#",
-    githubUrl: "https://github.com/ashishthomas/Online-Job-Portal.git",
-  },
-  {
-    id: 5,
     title: "FlowPay - Payment One Account App",
     description:
       "A modern payment application enabling secure transactions, fee calculation, and currency management with user-friendly dashboards for personal and business users.",
@@ -51,7 +30,7 @@ export const projects = [
     githubUrl: "https://github.com/ashishthomas/Payment-flow-app.git",
   },
   {
-    id: 6,
+    id: 4,
     title: "Nexora Tech Corporate Website",
     description:
       "A modern corporate website for Nexora Technologies, showcasing services and expertise with responsive design, fast performance, and seamless user experience for clients.",
